092c3e2393010d7850bae706bf3d8833ee260c50
Panuwat <pjanw001@fiu.edu>
2017-02-22 11:19:07 -0500

    ConnecCount can take multiple segment

--- main functions/eeg_connectivityCount.m
+++ main functions/eeg_connectivityCount.m
@@ -4,20 +4,43 @@ function Connection =  eeg_connectivityCount(eegConnectivityCounting, conMatrix 
 % This function finds number of connection within the assigned threshold
 %
 
-[~,thresSize] = size(threshold);
+[numThres,thresSize] = size(threshold);
 [~,~,maskSize] = size(eegConnectivityCounting.mask);
+
 Connection.threshold = threshold;
 Connection.location = eegConnectivityCounting.name;
 
-for i=1:1:thresSize
-    thresMatrix = conMatrix;
-    thresMatrix(conMatrix < threshold(i)) = 0;  
+if thresSize > 1
+    Connection.type = 'Inhibition and Excitation';
+    inhibitMatrix = conMatrix;
+    exciteMatrix = conMatrix;
     
-    for j=1:1:maskSize
-        temp = thresMatrix.*eegConnectivityCounting.mask(:,:,j);
-        [~,~,v] = find(temp>0);
-        Connection.numConnection(i,j) = sum(v);
+    
+    for i=1:1:numThres
+        exciteMatrix(exciteMatrix < threshold(i,2)) = 0;
+        inhibitMatrix(inhibitMatrix > threshold(i,1)) = 0;
+        for j=1:1:maskSize
+                tempInhibit = inhibitMatrix.*eegConnectivityCounting.mask(:,:,j);
+                tempExcite = exciteMatrix.*eegConnectivityCounting.mask(:,:,j);
+                [~,~,v1] = find(tempInhibit>0);
+                [~,~,v2] = find(tempExcite>0);
+                Connection.numConnectionInhibit(i,j) = sum(v1);
+                Connection.numConnectionExcite(i,j) = sum(v2);
+        end
     end
-end
+    
+else
+    Connection.type = 'Excitation';
+    for i=1:1:numThres
+        thresMatrix = conMatrix;
+        thresMatrix(conMatrix < threshold(i)) = 0;  
 
+        for j=1:1:maskSize
+            temp = thresMatrix.*eegConnectivityCounting.mask(:,:,j);
+            [~,~,v] = find(temp>0);
+            Connection.numConnection(i,j) = sum(v);
+        end
+    end
+
+end
 end
